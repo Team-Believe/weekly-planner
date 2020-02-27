0c0f8a7d8e0fcd1b127653cecc686c746d31b470
cassandraortiz <cassy.ortiz5@gmail.com>
2020-02-26 21:34:20 -0800

    local storage and event listener fix

--- js/app.js
+++ js/app.js
@@ -9,56 +9,52 @@ var mainUsersArr = [];
 //constructor for user
 function UserData(userName) {
   this.userName = userName;
-  // this.Meals = userMeals;
-  // this.Exercise = userExercise;
-  // this.ToDo = userToDo;
+  this.Meals = userMeals;
+  this.Exercise = userExercise;
+  this.ToDo = userToDo;
   mainUsersArr.push(this);
-
 }
 
 //constrcutor for user meals
-// function Meals(name) {
-//   this.title = name;
-//   this.ingredients = [];
-//   this.prepDirections = [];
-//   this.servings = 0;
-//   this.prepTime = 0;
-//   this.cookedTime = 0;
-//   userMeals.push(this);
-
-// }
+function Meals(name) {
+  this.title = name;
+  this.ingredients = [];
+  this.prepDirections = [];
+  this.servings = 0;
+  this.prepTime = 0;
+  this.cookedTime = 0;
+  userMeals.push(this);
+}
 
 //constructor for users exercise
-// function Exercise(name) {
-//   this.title = name;
-//   this.duration = 0;
-//   this.typeOfExcercise = '';
-//   this.reps = 0;
-//   this.calsBurned = 0;
-//   userMeals.push(this);
-
-// }
+function Exercise(name) {
+  this.title = name;
+  this.duration = 0;
+  this.typeOfExcercise = '';
+  this.reps = 0;
+  this.calsBurned = 0;
+  userMeals.push(this);
+}
 
 //contructor for users to do list
-// function ToDo(name) {
-//   this.title = name;
-//   this.list = [];
-//   userMeals.push(this);
-
-// }
+function ToDo(name) {
+  this.title = name;
+  this.list = [];
+  userMeals.push(this);
+}
 
 //function to look for existing user name
-// function findUser(name) {
-//   for (var i = 0; i < mainUsersArr.length; i++) {
-//     if (name === mainUsersArr[i].userName) {
-//       alert(`Welcome Back ${name}`);
-//       // break
-//     } else {
-//       alert('We need to create user');
-//     }
+function findUser(name) {
+  for (var i = 0; i < mainUsersArr.length; i++) {
+    if (name === mainUsersArr[i].userName) {
+      alert(`Welcome Back ${name}`);
+      // break
+    } else {
+      alert('We need to create user');
+    }
 
-//   }
-// }
+  }
+}
 
 //Event to get user name from input 
 var userNameEvent = document.getElementById('formUserName');
@@ -66,25 +62,55 @@ var userNameEvent = document.getElementById('formUserName');
 
 
 function handleSubmit(e) {
-  console.log('Inside the handle submit')
+
+  // Check if "Chuck is in the Main Arr users
+  // if Chuck is there ...
+  // --- PopulateUsers()
+  //--- pass through object index info
+  // if Chuck is NOT there ...
+  // --- new UserData()
+  // --- toLocalStorage()
+
+  console.log('Inside the handle submit');
   e.preventDefault();
-  new UserData('Roger');
+  // new UserData('Roger');
   console.log(e.target);
   var newUserOne = e.target.userName.value;
   new UserData(newUserOne);
+
   console.log(newUserOne);
 
-};
+  toLocalStorage();
+  populateUsers();
+}
 
-// new Meals('pasta');
-// new Meals('protein shake');
-// new Exercise('crossfit');
-// new Exercise('yoga');
+new Meals('pasta');
+new Meals('protein shake');
+new Exercise('crossfit');
+new Exercise('yoga');
 new UserData('Chuck');
 new UserData('Cassandra');
 new UserData('Lesley');
-new UserData('Rich')
+new UserData('Rich');
 
 // handleSubmit();
-console.log('Stating Event Listener')
-userNameEvent.addEventListener('sumbit', handleSubmit);
+console.log('Stating Event Listener');
+userNameEvent.addEventListener('submit', handleSubmit);
+
+function toLocalStorage(){
+  var stringArr = JSON.stringify(mainUsersArr);
+  localStorage.setItem('swMainUsers', stringArr);
+}
+
+function populateUsers(){
+  if (localStorage.getItem('swMainUsers')){
+    var allStoredUsers = JSON.parse(localStorage.getItem('swMainUsers'));
+
+    // for (var i = 0; i < allStoredUsers.length; i++) {
+    //   // new UserData()
+    // }
+    console.log(`there is something in local storage`);
+  } else {
+    console.log(`there is no local storage`);
+  }
+}
