6953e06bf5e3da8466547ca13a831a7939f76e44
Richard Whitehead <damian253@me.com>
2020-02-27 21:51:19 -0800

    updates

--- js/app.js
+++ js/app.js
@@ -5,16 +5,14 @@ var userExercise = [];
 var userToDo = [];
 var mainUsersArr = [];
 
-
 //constructor for user
-function UserData(userName) {
+function UserData(userName, userMeals, userExercise, userToDo) {
   this.userName = userName;
   this.Meals = userMeals;
   this.Exercise = userExercise;
   this.ToDo = userToDo;
   mainUsersArr.push(this);
 }
-
 //constrcutor for user meals
 function Meals(name) {
   this.title = name;
@@ -25,100 +23,157 @@ function Meals(name) {
   this.cookedTime = 0;
   userMeals.push(this);
 }
-
 //constructor for users exercise
 function Exercise(name) {
   this.title = name;
   this.duration = 0;
   this.typeOfExcercise = '';
   this.reps = 0;
-  this.calsBurned = 0;
-  userMeals.push(this);
+  this.calsToBurn = 0;
+  userExercise.push(this);
 }
-
 //contructor for users to do list
 function ToDo(name) {
   this.title = name;
   this.list = [];
-  userMeals.push(this);
+  userToDo.push(this);
 }
-
+var cIdx;
 //function to look for existing user name
 function findUser(name) {
+  var existingUser = false;
   for (var i = 0; i < mainUsersArr.length; i++) {
     if (name === mainUsersArr[i].userName) {
-      alert(`Welcome Back ${name}`);
-      // break
-    } else {
-      alert('We need to create user');
+      console.log('Welcome back dude');
+      existingUser = true;
+      cIdx = i;
+      break;
     }
-
+  }
+  if (existingUser === false) {
+    console.log('New user hola');
+    new UserData(name);
+    cIdx = mainUsersArr.length -1;
   }
 }
-
-<<<<<<< HEAD
-
-
-=======
-//Event to get user name from input 
+//Event to get user name from input
 var userNameEvent = document.getElementById('formUserName');
-
-
-
+userNameEvent.addEventListener('submit', handleSubmit);
 function handleSubmit(e) {
 
-  // Check if "Chuck is in the Main Arr users
-  // if Chuck is there ...
-  // --- PopulateUsers()
-  //--- pass through object index info
-  // if Chuck is NOT there ...
-  // --- new UserData()
-  // --- toLocalStorage()
-
-  console.log('Inside the handle submit');
+  // onclick;
   e.preventDefault();
-  // new UserData('Roger');
-  console.log(e.target);
-  var newUserOne = e.target.userName.value;
-  new UserData(newUserOne);
-
-  console.log(newUserOne);
-
+  var newUser = e.target.userName.value;
+  findUser(newUser);
   toLocalStorage();
-  populateUsers();
+  // window.location.href = 'main.html';
 }
-
->>>>>>> 8eeb54ac04873c623d5510afe94cd413230c89db
-new Meals('pasta');
-new Meals('protein shake');
-new Exercise('crossfit');
-new Exercise('yoga');
-new UserData('Chuck');
-new UserData('Cassandra');
-new UserData('Lesley');
-new UserData('Rich');
-<<<<<<< HEAD
-=======
-
-handleSubmit();
-console.log('Stating Event Listener');
-userNameEvent.addEventListener('submit', handleSubmit);
 
 function toLocalStorage(){
   var stringArr = JSON.stringify(mainUsersArr);
   localStorage.setItem('swMainUsers', stringArr);
 }
-
 function populateUsers(){
-  if (localStorage.getItem('swMainUsers')){
+  if (localStorage.getItem('swMainUsers')) {
+    console.log('there is something in local storage');
     var allStoredUsers = JSON.parse(localStorage.getItem('swMainUsers'));
-
-    // for (var i = 0; i < allStoredUsers.length; i++) {
-    //   // new UserData()
-    // }
-    console.log(`there is something in local storage`);
+    console.log(allStoredUsers);
+    for (var i = 0; i < allStoredUsers.length; i++) {
+      new UserData(allStoredUsers[i].userName, allStoredUsers[i].userMeals, allStoredUsers[i].userExercise, allStoredUsers[i].userToDo);
+    }
   } else {
-    console.log(`there is no local storage`);
+    console.log('there is no local storage');
+    new Meals('pasta');
+    new Meals('protein shake');
+    new Exercise('crossfit');
+    new Exercise('yoga');
+    new UserData('Chuck', userMeals);
+    new UserData('Cassandra',[],userExercise);
+    new UserData('Lesley', userMeals, userExercise);
+    new UserData('Rich', userMeals, userExercise);
   }
 }
->>>>>>> 8eeb54ac04873c623d5510afe94cd413230c89db
+populateUsers();
+// Global Variables of Elements Needed
+var applyTask = document.getElementById('addNewTask_Form');
+var taskCategory = document.getElementById('taskCategory');
+var timeDropdown = document.getElementById('daytimeSelected');
+var dropDownSection = document.getElementById('timeOfDay_Dropdown');
+
+//EVENT: Apply li to the Weekly Planner based on Days checked
+// - the time of day selected will place in the corresponding ul
+// - adds the task name to the li
+// - gives the li an ID of: '[Category]Task' - for styling
+//============================================================
+applyTask.addEventListener('submit', findDaysApplied);
+
+function findDaysApplied(e){
+  e.preventDefault();
+  var category = e.target.taskCategory.value;
+  var timeOfDay = e.target.daytimeSelected.value;
+  var taskEntered = e.target.taskEntered.value;
+
+  for(var x = 1; x <= 7; x++){
+
+    var dayChecked = document.getElementById(`day${x}_chkbx`);
+    var postSection  = document.getElementById(`day${x}_${timeOfDay}`);
+
+    if (dayChecked.checked){
+      var taskItem = document.createElement('li');
+      taskItem.textContent = `${taskEntered}`;
+      taskItem.id = `${category}Task`;
+      postSection.appendChild(taskItem);
+    }
+  }
+
+  applyTask.reset();
+  dropDownSection.style.display = 'none';
+}
+//https://www.w3schools.com/howto/howto_js_todolist.asp
+
+
+//EVENT: Show/Hide Time of Day Dropdown Menu
+// - This event will also populate the dropdown
+// - based off the Category selected.  This gives the
+// - option for the Meals to be different.
+// - Checkbox value is still the same to coincide with the planner lists
+//=================================================
+taskCategory.addEventListener('change', dayDropdownList);
+
+function dayDropdownList(e){
+  e.preventDefault();
+  var cat = taskCategory.value;
+  var mealArr = ['Breakfast','Lunch','Dinner'];
+  var valueArr = ['morning','afternoon','evening'];
+
+  while(timeDropdown.childElementCount > 0) {
+    timeDropdown.removeChild(timeDropdown.lastElementChild);
+  }
+
+  if(cat === 'Meal'){
+    dropDownSection.style.display = 'inline-flex';
+    for(var i = 0; i<mealArr.length; i++){
+      var selection = document.createElement('option');
+      selection.textContent = mealArr[i];
+      selection.value = valueArr[i];
+      timeDropdown.appendChild(selection);
+    }
+  } else if (cat === ''){
+    dropDownSection.style.display = 'none';
+    while(timeDropdown.childElementCount > 0) {
+      timeDropdown.removeChild(timeDropdown.lastElementChild);
+    }
+  }else {
+    dropDownSection.style.display = 'inline-flex';
+    for(var j = 0; j<valueArr.length; j++){
+      selection = document.createElement('option');
+      selection.textContent = valueArr[j];
+      selection.value = valueArr[j];
+      timeDropdown.appendChild(selection);
+    }
+  }
+}
+
+
+
+
